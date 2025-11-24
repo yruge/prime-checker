1372e4952d1499bcb8c20b63caf5a28165b95ed3
yruge <jasonjthen@gmail.com>
2025-11-24 19:36:52 +0700

    corrected logic & created input page

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,28 @@
+import isPrime from './prime-logic.js';
 
 
 export default function Home() {
+  function handleCheck() {
+    const input = document.querySelector('#number-input');
+    const convertedNum = parseInt(input.value, 10);
+    const check = isPrime(convertedNum);
+
+    if(check)
+    {
+      alert(`${input.value} is a prime number.`);
+    } 
+    else if(!check)
+    {
+      alert(`${input.value} is not a prime number.`);
+    }
+  }
+
+
   return (
     <div>
+      <h1>Prime Number Checker</h1>
+      <input type="number" id="number-input" placeholder="Enter a number"/>
+      <button onClick={handleCheck}>Check</button>
     </div>
   );
 }
--- src/app/prime-logic.js
+++ src/app/prime-logic.js
@@ -1,9 +1,9 @@
 
-export function isPrime(num) {
+export default function isPrime(num) {
     if(num <= 1) return false;
     if(num === 2) return true;
 
-    if(num % num === 0 && num % 1 === 0)
+    if(num % num === 0 && num % 1 === num)
     {
         let factorNum = [];
         for(let i = 1 ; i <= num ; i++)
